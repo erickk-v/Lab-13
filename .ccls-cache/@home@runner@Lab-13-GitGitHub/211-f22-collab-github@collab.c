03ac95427461f8b9716fd04be5869b4cf9b71b5f
Erick Vazquez <118034500+erickk-v@users.noreply.github.com>
2022-11-16 21:04:35 +0000

    added new repo

--- .ccls-cache/@home@runner@Lab-13-GitGitHub/211-f22-collab-github@collab.c
+++ .ccls-cache/@home@runner@Lab-13-GitGitHub/211-f22-collab-github@collab.c
@@ -1,19 +1,18 @@
 // Collaborative code file, Lab 13
-//
+// 
 // One person in your partner group should clone this repository.
-// Create a function that outputs your name, along with either one thing
+// Create a function that outputs your name, along with either one thing 
 //    that you learned about git or GitHub or something that gave you trouble
 //    that you would like others to learn from. The function name should be
 //    your NetID combined with your partner's NetID.
 // Feel free to add other things to your function as well, though be mindful
-// of the length of it, given how many students will be collaborating on this
-// code!
-//
+// of the length of it, given how many students will be collaborating on this code!
+// 
 // Read through the code that is already here before adding your own.
 // Add this code to the repository using git commands.
-//
+// 
 // An example is shown below.
-//
+// 
 // UIC CS 211, Fall 2022
 #include <stdio.h>
 
@@ -21,19 +20,12 @@ void reed_ekidan2() {
   printf("Dale Reed and Ellen Kidane\n");
   printf("We learned that git and GitHub are amazing, powerful tools!\n");
 }
-void evazq2_schit7() {
-  printf("Erick Vazquez and Sai Chittala");
-  printf("We learned how to clone the repository using git commands.");
-  printf("We learned the difference between git and github. Where git is a "
-         "version control system, and github being a hosting platform for "
-         "collaboration.");
-}
 
-int main() {
-  printf(
-      "This code has been created by the students of CS 211, Fall 2022!\n\n");
-
-  reed_ekidan2();
-
-  return 0;
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    reed_ekidan2();
+    
+    return 0;
 }
